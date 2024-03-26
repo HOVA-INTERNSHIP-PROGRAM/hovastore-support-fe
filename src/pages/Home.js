0382e39cb95bcd4b96e8e0d3dcb9fc9fda3b80e0
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 <anzymen@gmail.com>
2024-03-26 12:14:32 +0200

    done with FAQs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Accordion from "../components/accordion/accordion";
 
 const Home = () => {
   return (
-    <div className='text-2xl'>This is landing page</div>
-  )
-}
+    <div className="text-2xl">
+      This is landing page
+      <div>
+        <Accordion />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
