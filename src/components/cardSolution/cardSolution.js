1f1a6e1f9b85fb01baad915112595ae2e5148749
Ericsibomana <ericsibomana433@gmail.com>
2024-03-30 17:33:34 +0200

    fix error on card solution

---
+++ src/components/cardSolution/cardSolution.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function CardSolution({ icon, title, description }) {
+  return (
+      <div role="status" className="bg-secondary shadow-md rounded-md p-5 hover:scale-105 transition ease-out duration-200 hover:shadow-xl">
+        <div className="flex justify-center p-1">
+        <div className="flex justify-center items-center rounded-full bg-secondaryLight w-16 h-16 text-primary font-bold">
+        <div className="flex justify-center items-center w-8 h-8">
+            <img src={icon} alt="icon" />
+        </div>
+      </div>
+        </div>
+        <h3 className="text-primary text-lg text-center mt-4 mb-3 font-bold">{title}</h3>
+        <p className="text-black text-xs text-center fon-light">{description}</p>
+    </div>
+  );
+}
+
+export default CardSolution;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import CardSolution from '../components/cardSolution/cardSolution'
+import cardsData from '../data/data'
 
 const Home = () => {
   return (
-    <div className='text-2xl'>I have created new working flow</div>
+    <div className='text-2xl'>
+      <div className='bg-secondaryLight flex justify-center items-center'>
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 w-3/4 gap-5 pt-10 pb-8'>
+      {cardsData.map((card, index) => (
+        <CardSolution
+          key={index}
+          icon={card.icon}
+          title={card.title}
+          description={card.description}
+        />
+      ))}
+    </div>
+    </div>
+
+    </div>
   )
 }
 
