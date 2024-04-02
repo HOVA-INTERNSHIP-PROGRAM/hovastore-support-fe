1dbc148c2977646a16045a69ddd08a8c8d0a7380
wayneleon1 <rurangwaleon10@gmail.com>
2024-04-02 23:02:57 +0200

    Fix styles for slug page

--- src/layouts/SlugLayout.js
+++ src/layouts/SlugLayout.js
@@ -7,12 +7,12 @@ function SlugLayout() {
       <div className="mb-8 p-2 bg-white inline-block rounded-sm">
         Breadcrumbs goes Here...
       </div>
-      <div className="grid grid-cols-5 gap-8">
-        <div className="col-span-1 h-20 bg-white p-4">Categories Menu</div>
-        <div className="col-span-3 h-20 bg-white rounded-sm">
+      <div className="grid grid-cols-5 gap-6">
+        <div className="col-span-1  bg-white p-4">Categories Menu</div>
+        <div className="col-span-3  bg-white rounded-sm p-4">
           <Outlet />
         </div>
-        <div className="col-span-1 h-20 bg-white p-4 rounded-sm">
+        <div className="col-span-1  bg-white p-4 rounded-sm">
           related topics
         </div>
       </div>
