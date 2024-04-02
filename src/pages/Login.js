a6e55c46b35f234ab515394ee9978e36f8b48c75
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 <anzymen@gmail.com>
2024-04-02 21:48:46 +0200

    working with reusable components

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,8 @@
 import React from "react";
 import image from "../assets/images/loginImage.png";
 import { Link } from "react-router-dom";
+import Input from "../components/UI/Input";
+import Button from "../components/UI/Button";
 
 function Login() {
   return (
@@ -12,14 +14,25 @@ function Login() {
         <div className="w-[80%] h-full flex flex-col justify-start items-center">
           <h2 className="text-[40px] font-extrabold text-primary">Login</h2>
           <div className="bg-slate-600 w-full mt-[82px] ">
-            <input type="text" placeholder="enter username" className="w-full p-2 border-[2px]"/>
+            <input
+              type="text"
+              placeholder="enter username"
+              className="w-full p-2 border-[2px]"
+            />
           </div>
           <div className="mt-[34px] w-full">
-            <input type="text" placeholder="enter username" className="w-full p-2 border-[2px]"/>
+            {/* <input type="text" placeholder="enter username" className="w-full p-2 border-[2px]"/> */}
+            <Input
+              type="input"
+              placeholder="Password"
+              text="password"
+              label={`pssword`}
+            />
           </div>
-          <button className="mt-[34px] bg-primary w-full h-[51px] text-secondary rounded-[5px] font-bold text-[20px]">
+          <Button title={`Login`} styles={`w-full !scale-100`} />
+          {/* <button className="mt-[34px] bg-primary w-full h-[51px] text-secondary rounded-[5px] font-bold text-[20px]">
             Login
-          </button>
+          </button> */}
           <div className="mt-[44px] flex flex-row justify-between w-full text-[20px]">
             <Link to="/register">create account</Link>
             <Link to="/forgot-password">Forgot password ?</Link>
