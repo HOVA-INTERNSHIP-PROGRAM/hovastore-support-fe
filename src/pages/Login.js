16c9a9d6d721176b47eb576064e0c7b1263ec99c
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 <anzymen@gmail.com>
2024-04-09 16:53:29 +0200

    worked on login, forgot password and removed image from there as required

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,4 @@
 import React from "react";
-import image from "../assets/images/loginImage.png";
 import { Link } from "react-router-dom";
 import Input from "../components/UI/HsInput";
 import Button from "../components/UI/HsButton";
@@ -8,14 +7,12 @@ import { FaUser, FaLock } from "react-icons/fa";
 
 function Login() {
   return (
-    <div className="bg-secondary w-full h-dvh grid md:grid-cols-1 xl:grid-cols-2 xl:gap-20">
-      <div className="hidden w-auto h-full md:justify-self-end xl:flex flex-col justify-center">
-        <img src={image} alt="loginImage" />
-      </div>
-      <div className="w-full flex flex-col justify-center items-center xl:items-start">
+    <div className="bg-secondary w-full h-dvh flex justify-center items-center">
+      <div className="w-full flex flex-col justify-center items-center">
         <div className="bg-white w-[90%] md:w-486 h-620 rounded-sm flex justify-center items-center py-10 shadow-md">
-          <div className="w-[95%] md:w-[85%] h-full flex flex-col justify-start items-center">
-            <h2 className="text-[40px] md:text-[48px] font-extrabold text-primary">Login</h2>
+
+          <div className="w-[95%] md:w-[85%] h-full flex flex-col justify-center items-center">
+            <h2 className="text-2xl lx:text-4xl font-extrabold text-primary">HAVASTORE SUPPORT</h2>
             <div className="mt-6 mdl:mt-[34px] w-full">
               <Input type="input" placeholder="Username" icon={<FaUser />} />
             </div>
@@ -31,10 +28,7 @@ function Login() {
               title={`Login`}
               styles={`w-full !scale-100 mt-6 mdl:mt-12 bg-primary text-white`}
             />
-            <div className="mt-6 mdl:mt-[44px] flex flex-col md:flex-row items-center md:items-start md:justify-between w-full text-[20px] md:text-[24px]">
-              <Link to="/register" className="hover:underline text-[#3558D4]">
-                create account
-              </Link>
+            <div className="mt-6 mdl:mt-[44px] flex flex-col md:flex-row items-center md:items-center md:justify-center w-full text-[20px] md:text-[24px]">
               <Link
                 to="/forgot-password"
                 className="hover:underline text-[#3558D4]"
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,4 @@
 import React from "react";
-import image from "../assets/images/loginImage.png";
 import { Link } from "react-router-dom";
 import Input from "../components/UI/HsInput";
 import Button from "../components/UI/HsButton";
@@ -8,12 +7,10 @@ import { FaUser, FaLock } from "react-icons/fa";
 
 function Login() {
   return (
-    <div className="bg-secondary w-full h-dvh grid md:grid-cols-1 xl:grid-cols-2 xl:gap-20">
-      <div className="hidden w-auto h-full md:justify-self-end xl:flex flex-col justify-center">
-        <img src={image} alt="loginImage" />
-      </div>
-      <div className="w-full flex flex-col justify-center items-center xl:items-start">
+    <div className="bg-secondary w-full h-dvh flex justify-center items-center">
+      <div className="w-full flex flex-col justify-center items-center">
         <div className="bg-white w-[90%] md:w-486 h-620 rounded-sm flex justify-center items-center py-10 shadow-md">
+
           <div className="w-[95%] md:w-[85%] h-full flex flex-col justify-start items-center">
             <h2 className="text-[40px] md:text-[48px] font-extrabold text-primary">Register</h2>
             <div className="mt-6 mdl:mt-[34px] w-full">
