80951690b7402ec497ff2bb4d0e8af04bfe4d5d4
wayneleon1 <rurangwaleon10@gmail.com>
2024-04-15 23:22:50 +0200

    create breadcrumbs component & artical page

--- src/components/cardSolution/cardSolution.js
+++ src/components/cardSolution/cardSolution.js
@@ -7,7 +7,7 @@ function CardSolution({ icon, title, description, click }) {
       onClick={click}
       to={`/${title}`}
       role="status"
-      className="bg-secondary shadow-md rounded-md p-5 hover:scale-105 transition ease-out duration-200 hover:shadow-xl cursor-pointer"
+      className="bg-white shadow-md rounded-md p-5 hover:scale-105 transition ease-out duration-200 hover:shadow-xl cursor-pointer"
     >
       <div className="flex justify-center p-1">
         <div className="flex justify-center items-center rounded-full bg-secondaryLight w-16 h-16 text-primary font-bold">
---
+++ src/dummy/data.js
@@ -0,0 +1,128 @@
+export const data = [
+  {
+    icon: "Icon",
+    title: "Customers",
+    desc: "add Customers in your system",
+    subcategories: [
+      {
+        subTitle: "Customers Management",
+        articals: [
+          {
+            articalTitle: "How to do Customers in this app",
+            content:
+              "This is the steps that could helps you while you are will to do Customers ",
+          },
+          {
+            articalTitle: "How to cancel progress of Customers",
+            content:
+              "This is the steps that could helps you while you are will to cancel Customers ",
+          },
+        ],
+      },
+      {
+        subTitle: "Customers Progress & Monitaring",
+        articals: [
+          {
+            articalTitle: "How to do Customers in this app",
+            content:
+              "This is the steps that could helps you while you are will to do payment ",
+          },
+          {
+            articalTitle: "How to enhance your products ",
+            content:
+              "This is the steps that could helps you while you are will to cancel payment ",
+          },
+          {
+            articalTitle: "How to add new payment account",
+            content:
+              "This is the steps that could helps you while you are will to cancel payment ",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    icon: "Icon",
+    title: "Payment",
+    desc: "add payment in your system",
+    subcategories: [
+      {
+        subTitle: "Payment Management",
+        articals: [
+          {
+            articalTitle: "How to do payment in this app",
+            content:
+              "This is the steps that could helps you while you are will to do payment ",
+          },
+          {
+            articalTitle: "How to cancel progress of payment",
+            content:
+              "This is the steps that could helps you while you are will to cancel payment ",
+          },
+        ],
+      },
+      {
+        subTitle: "Payment Progress & Monitaring",
+        articals: [
+          {
+            articalTitle: "How to do payment in this app",
+            content:
+              "This is the steps that could helps you while you are will to do payment ",
+          },
+          {
+            articalTitle: "How to enhance your products ",
+            content:
+              "This is the steps that could helps you while you are will to cancel payment ",
+          },
+          {
+            articalTitle: "How to add new payment account",
+            content:
+              "This is the steps that could helps you while you are will to cancel payment ",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    icon: "Icon",
+    title: "Customer",
+    desc: "add payment in your system",
+    subcategories: [
+      {
+        subTitle: "Payment Management",
+        articals: [
+          {
+            articalTitle: "How to do payment in this app",
+            content:
+              "This is the steps that could helps you while you are will to do payment ",
+          },
+          {
+            articalTitle: "How to cancel progress of payment",
+            content:
+              "This is the steps that could helps you while you are will to cancel payment ",
+          },
+        ],
+      },
+      {
+        subTitle: "Payment Progress & Monitaring",
+        articals: [
+          {
+            articalTitle: "How to do payment in this app",
+            content:
+              "This is the steps that could helps you while you are will to do payment ",
+          },
+          {
+            articalTitle: "How to enhance your products ",
+            content:
+              "This is the steps that could helps you while you are will to cancel payment ",
+          },
+          {
+            articalTitle: "How to add new payment account",
+            content:
+              "This is the steps that could helps you while you are will to cancel payment ",
+          },
+        ],
+      },
+    ],
+  },
+];
