b38c29770cd4ea4e960d43aeaac8990e0f99de60
wayneleon1 <rurangwaleon10@gmail.com>
2024-04-25 00:04:02 +0200

    create content for tabs

--- src/pages/dashboard/Settings.js
+++ src/pages/dashboard/Settings.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Profile from "../../assets/images/loginImage.png";
 
 function Settings() {
   const [toggleState, setToggleState] = useState(1);
@@ -9,6 +10,7 @@ function Settings() {
 
   return (
     <div className="bg-white p-8 h-[400px] rounded-sm">
+      {/* Tab Container */}
       <div className="flex gap-10 items-center font-medium border-b-[1px] border-[#F4F5F7]">
         <div
           className="cursor-pointer flex flex-col gap-2"
@@ -35,6 +37,28 @@ function Settings() {
                 : "bg-white h-1"
             }
           ></div>
+        </div>
+      </div>
+      {/* Tab content container */}
+      <div>
+        {/* Edit Profile container */}
+        <div className={toggleState === 1 ? "flex gap-10 mt-8" : "hidden"}>
+          <div className="w-[100px] h-[100px] rounded-full  relative">
+            <img
+              src={Profile}
+              alt="profile-image"
+              className="w-full h-full object-cover rounded-full"
+            />
+            <div className="absolute w-6 h-6 rounded-full bg-[#2E63EC] bottom-5 right-4 z-10">
+              ed
+            </div>
+          </div>
+          <div>Form</div>
+        </div>
+
+        {/* Security container */}
+        <div className={toggleState === 2 ? "flex mt-8" : "hidden"}>
+          <div>security</div>
         </div>
       </div>
     </div>
