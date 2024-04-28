7f6a914dd40693fe6d8751dfaf7d696fbdd34e7b
wayneleon1 <rurangwaleon10@gmail.com>
2024-04-28 16:51:07 +0200

    adds CKeditor package

--- src/pages/dashboard/NewArticle.js
+++ src/pages/dashboard/NewArticle.js
@@ -1,7 +1,49 @@
 import React from "react";
+import HSInput from "../../components/form/HSInput";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 
 function NewArticle() {
-  return <div>NewArticle</div>;
+  return (
+    <div className="md:p-8">
+      <div
+        className="flex flex-col gap-5
+      "
+      >
+        <HSInput
+          type="input"
+          label="Article Title"
+          placeholder="Enter title"
+          style={`lg:w-[400px]`}
+        />
+        <HSInput
+          type="input"
+          label="Article Category"
+          placeholder="Choose category"
+          style={`lg:w-[400px]`}
+        />
+        <label for="ckeditor">Write full article</label>
+        <CKEditor
+          id="ckeditor"
+          editor={ClassicEditor}
+          data="<p>Hello from CKEditor&nbsp;5!</p>"
+          onReady={(editor) => {
+            // You can store the "editor" and use when it is needed.
+            console.log("Editor is ready to use!", editor);
+          }}
+          onChange={(event) => {
+            console.log(event);
+          }}
+          onBlur={(event, editor) => {
+            console.log("Blur.", editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log("Focus.", editor);
+          }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default NewArticle;
